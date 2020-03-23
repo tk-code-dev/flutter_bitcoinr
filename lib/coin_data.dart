eb7aa89d5ac9b463edb4d5c733ce61b5dfe5a3a7
tkcode <tkcode.dev@gmail.com>
2020-03-23 16:03:34 -0400

    Fetch rate using coinAPI, http flutter package

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,9 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '25E4B59D-2963-4EF4-B314-CB563CE14900';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +34,27 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  //  Create the Asynchronous method getCoinData() that returns a Future (the price data).
+  Future getCoinData() async {
+    //  Create a url combining the coinAPIURL with the currencies, BTC to USD.
+    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    //  Make a GET request to the URL and wait for the response.
+    http.Response response = await http.get(requestURL);
+
+    //  Check that the request was successful.
+    if (response.statusCode == 200) {
+      //  Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
+      var decodedData = jsonDecode(response.body);
+      //  Get the last price of bitcoin with the key 'last'.
+      var lastPrice = decodedData['rate'];
+      //  Output the lastPrice from the method.
+      return lastPrice;
+    } else {
+      //  Handle any errors that occur during the request.
+      print(response.statusCode);
+      //  Optional: throw an error if our request fails.
+      throw 'Problem with the get request';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -11,6 +11,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  String bitcoinValueInUSD = '?';
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -24,10 +25,13 @@ class _PriceScreenState extends State<PriceScreen> {
     }
     return DropdownButton(
       value: selectedCurrency,
-      icon: Icon(Icons.arrow_downward),
+      icon: Icon(Icons.expand_more),
       iconSize: 30,
       elevation: 40,
-      style: TextStyle(color: Colors.orange),
+      style: TextStyle(
+        color: Colors.white70,
+        fontSize: 30.0,
+      ),
       items: dropdownItems,
       onChanged: (value) {
         setState(
@@ -54,45 +58,73 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
+  //  Create an async method here await the coin data from coin_data.dart
+  void getData() async {
+    try {
+      double data = await CoinData().getCoinData();
+      //  can't await in a setState(). So you have to separate it out into two steps.
+      setState(() {
+        bitcoinValueInUSD = data.toStringAsFixed(2);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //  Call getData() when the screen loads up. We can't call CoinData().getCoinData() directly here because can't make initState() async.
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true, //  Center the title of an appbar
         title: Text('🤑 Coin Ticker'),
+        backgroundColor: Colors.black,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ?? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+      body: Container(
+        color: Colors.black87,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+              child: Card(
+                color: Colors.black12,
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                  child: Text(
+                    //  Update the Text Widget with the data in bitcoinValueInUSD.
+                    '1 BTC = $bitcoinValueInUSD USD',
+
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
-          ),
-        ],
+            Container(
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.black54,
+              child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+            ),
+          ],
+        ),
       ),
     );
   }
