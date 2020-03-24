cbc274cc28f1d91c08a75f34f0d920e9a3bd9c22
tkcode <tkcode.dev@gmail.com>
2020-03-23 20:24:13 -0400

    Add value with commas

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -35,26 +35,25 @@ const List<String> cryptoList = [
 ];
 
 class CoinData {
-  //  Create the Asynchronous method getCoinData() that returns a Future (the price data).
   Future getCoinData(String selectedCurrency) async {
-    //  Create a url combining the coinAPIURL with the currencies, BTC to USD.
-    String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey';
-    //  Make a GET request to the URL and wait for the response.
-    http.Response response = await http.get(requestURL);
-
-    //  Check that the request was successful.
-    if (response.statusCode == 200) {
-      //  Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
-      var decodedData = jsonDecode(response.body);
-      //  Get the last price of bitcoin with the key 'last'.
-      var lastPrice = decodedData['rate'];
-      //  Output the lastPrice from the method.
-      return lastPrice;
-    } else {
-      //  Handle any errors that occur during the request.
-      print(response.statusCode);
-      //  Optional: throw an error if our request fails.
-      throw 'Problem with the get request';
+    //4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    //5: Return a Map of the results instead of a single value.
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      //Update the URL to use the crypto symbol from the cryptoList
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(requestURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
     }
+    return cryptoPrices;
   }
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -11,7 +11,6 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
-  String bitcoinValueInUSD = '?';
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -37,7 +36,7 @@ class _PriceScreenState extends State<PriceScreen> {
         setState(
           () {
             selectedCurrency = value;
-            //2: Call getData() when the picker/dropdown changes.
+            //  Call getData() when the picker/dropdown changes.
             getData();
           },
         );
@@ -48,7 +47,10 @@ class _PriceScreenState extends State<PriceScreen> {
   CupertinoPicker iOSPicker() {
     List<Text> pickerItems = [];
     for (String currency in currenciesList) {
-      pickerItems.add(Text(currency));
+      pickerItems.add(Text(
+        currency,
+        style: TextStyle(color: Colors.white),
+      ));
     }
 
     return CupertinoPicker(
@@ -66,13 +68,21 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  //  Create an async method here await the coin data from coin_data.dart
+  //  value had to be updated into a Map to store the values of all three cryptocurrencies.
+  Map<String, String> coinValues = {};
+  //  Figure out a way of displaying a '?' on screen while we're waiting for the price data to come back. First we have to create a variable to keep track of when we're waiting on the request to complete.
+  bool isWaiting = false;
+
   void getData() async {
+    //  Second, we set it to true when we initiate the request for prices.
+    isWaiting = true;
     try {
-      double data = await CoinData().getCoinData(selectedCurrency);
-      //  can't await in a setState(). So you have to separate it out into two steps.
+      //  Update this method to receive a Map containing the crypto:price key value pairs.
+      var data = await CoinData().getCoinData(selectedCurrency);
+      //  Third, as soon the above line of code completes, we now have the data and no longer need to wait. So we can set isWaiting to false.
+      isWaiting = false;
       setState(() {
-        bitcoinValueInUSD = data.toStringAsFixed(0);
+        coinValues = data;
       });
     } catch (e) {
       print(e);
@@ -91,47 +101,98 @@ class _PriceScreenState extends State<PriceScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true, //  Center the title of an appbar
-        title: Text('🤑 Coin Ticker'),
+        title: Text('Coin Ticker'),
         backgroundColor: Colors.black,
       ),
       body: Container(
         color: Colors.black87,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-              child: Card(
-                color: Colors.black12,
-                elevation: 5.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                  child: Text(
-                    //  Update the Text Widget with the data in bitcoinValueInUSD.
-                    '1 BTC = $bitcoinValueInUSD /BTC $selectedCurrency',
-
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    ),
+            //  use a Column Widget to contain the three CryptoCards.
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  CryptoCard(
+                    cryptoCurrency: 'BTC',
+                    //  Finally, use a ternary operator to check if waiting and if so, display a '?' otherwise show the actual price data.
+                    value: isWaiting
+                        ? '?'
+                        : coinValues['BTC'].replaceAllMapped(
+                            new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+                            (Match m) => '${m[1]},'),
+                    selectedCurrency: selectedCurrency,
                   ),
-                ),
+                  CryptoCard(
+                    cryptoCurrency: 'ETH',
+                    value: isWaiting
+                        ? '?'
+                        : coinValues['ETH'].replaceAllMapped(
+                            new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+                            (Match m) => '${m[1]},'),
+                    selectedCurrency: selectedCurrency,
+                  ),
+                  CryptoCard(
+                    cryptoCurrency: 'LTC',
+                    value: isWaiting
+                        ? '?'
+                        : coinValues['LTC'].replaceAllMapped(
+                            new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+                            (Match m) => '${m[1]},'),
+                    selectedCurrency: selectedCurrency,
+                  ),
+                ],
               ),
             ),
             Container(
               height: 150.0,
-              alignment: Alignment.center,
+              alignment: Alignment.bottomCenter,
               padding: EdgeInsets.only(bottom: 30.0),
               color: Colors.black54,
               child: Platform.isIOS ? iOSPicker() : androidDropdown(),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+//  Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
+class CryptoCard extends StatelessWidget {
+  //  You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
+  const CryptoCard({
+    this.value,
+    this.selectedCurrency,
+    this.cryptoCurrency,
+  });
+
+  final String value;
+  final String selectedCurrency;
+  final String cryptoCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoCurrency = $value $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.black54,
+            ),
+          ),
         ),
       ),
     );
